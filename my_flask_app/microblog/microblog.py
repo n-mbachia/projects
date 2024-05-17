d5ace3b0a17013f4818c4292f0f3e5d38a19d39f
Mbachia Ng'ethe <ndmbachia@outlook.com>
2024-05-17 06:33:01 +0300

    all changed made

--- my_flask_app/microblog/microblog.py
+++ my_flask_app/microblog/microblog.py
@@ -1,7 +1,10 @@
 # microblog.py
 
-from app import app
-"""
-app = Flask(__name__)
-app.config.from_object(Config)
-"""
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor # register function as a shell context function
+def make_shell_context():
+    return{'sa': sa, 'so':so, 'db':db, 'User':User, 'Post':Post}
