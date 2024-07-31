fe4905916f76370ac1b4b492d0faab8ceede2a6b
Mbachia Ng'ethe <ndmbachia@outlook.com>
2024-07-31 07:38:27 +0300

    init file removed

--- my_flask_app/mn_pikngo_app/app.py
+++ my_flask_app/mn_pikngo_app/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 from flask_ckeditor import CKEditor
@@ -31,6 +31,26 @@ csrf.exempt(blueprint)  # Use with caution
 
 # Register the Blueprint
 app.register_blueprint(blueprint, url_prefix='/')
+
+########################
+#### error handlers ####
+########################
+
+
+@app.errorhandler(401)
+def unauthorized_page(error):
+    return render_template("errors/401.html"), 401
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("errors/404.html"), 404
+
+
+@app.errorhandler(500)
+def server_error_page(error):
+    return render_template("errors/500.html"), 500
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
